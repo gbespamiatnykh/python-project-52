3e6dbcb29998ee29b974ca1d4e410faa7e943956
George Bespamiatnykh <george.bespamiatnykh@gmail.com>
2026-03-17 02:22:45 +0700

    feat: initialize Django project

---
+++ task_manager/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(
+        request,
+        "index.html",
+        context={
+            "text": "Hello, World!",
+        },
+    )
